31f552f8641be1625ab86384059f534c4eedd498
59db <AkirusG@Gmail.com>
2013-01-29 15:36:23 +0300

    update to 1.1

--- src/org/sinrel/adblock/AdBlock.java
+++ src/org/sinrel/adblock/AdBlock.java
@@ -1,12 +1,16 @@
 package org.sinrel.adblock;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.defaults.BanCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -25,6 +29,8 @@ public class AdBlock extends JavaPlugin implements Listener{
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent event){
 		Player player = event.getPlayer();
+		if (player.isOp() || player.hasPermission("adBlock.admin")) return;
+		
 		for(Pattern p : patterns){
 			Matcher m = p.matcher(event.getMessage());
 			if(m.matches()){		
@@ -46,10 +52,29 @@ public class AdBlock extends JavaPlugin implements Listener{
 
 	@Override
 	public void onEnable() {
-		loadConfig();
 		this.getServer().getPluginManager().registerEvents(this, this);
+		if(!new File(this.getDataFolder(),"config.yml").exists()) createConfig();
+		loadConfig();
 	}
 	
+	private void createConfig() {
+		FileConfiguration config = this.getConfig();
+		List<String> regs = new ArrayList<String>();
+		regs.add(".*[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}.*");
+		regs.add(".*[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.*");
+		regs.add(".*[a-zA-z]+\\.(com|net|ru|ua|by|info|us|uk|so|org|su|tv|kz).*");
+		regs.add(".*[a-zA-z]+\\.(com|net|ru|ua|by|info|us|uk|so|org|su|tv|kz):[0-9]{1,5}.*");
+		
+		config.set("regulars", regs);
+		config.set("banEnabled", false);
+		config.set("kickEnabled", true);
+		config.set("kickMessage", "commercial is not allowed in this server");
+		config.set("replaceEnabled", true);
+		config.set("replaceText", "commercial");
+		
+		saveConfig();
+	}
+
 	private void loadConfig()
 	{
 		FileConfiguration config = this.getConfig();
@@ -62,7 +87,6 @@ public class AdBlock extends JavaPlugin implements Listener{
 		
 		if(regexps == null) return;
 		for(String s : regexps){
-			System.out.println(s);
 			patterns.add(Pattern.compile(s));
 		}
 	}
