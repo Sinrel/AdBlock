f4fb960915e0088825346144f30830b6bed9d2f9
Sarkhad <sarkhad@gmail.com>
2013-02-04 21:57:59 +0300

    Update to 1.2
    little changes ;)

--- src/org/sinrel/adblock/AdBlock.java
+++ src/org/sinrel/adblock/AdBlock.java
@@ -1,12 +1,9 @@
 package org.sinrel.adblock;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.defaults.BanCommand;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
